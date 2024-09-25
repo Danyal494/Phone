ffafb5e4c8b6e00cc2607517f8f3d53ca4e80841
Danyal494 <dk7167301@gmail.com>
2024-09-25 17:14:42 +0500

    add blend

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
-import Task from './components/Task'
+import Tasks from './components/Tasks'
 import ITask from './components/ITask'
 import Btn from './components/Btn'
 
@@ -15,7 +14,7 @@ function App() {
 
   return (
     <div className='bg-[#42A5F5]' style={{ position: "relative", height: "100vh", width: "100vw" }}>
-      {open ? <Btn endLoading={handleBtn}/> : <Task style={{ position: "absolute", top: 20, left: 0, zIndex: 10 }}/> }
+      {open ? <Btn endLoading={handleBtn}/> : <Tasks style={{ position: "absolute", top: 20, left: 0, zIndex: 10 }}/> }
      
       <ITask style={{ position: "absolute", top: 0, left: 0, zIndex: 5 }}/>
     </div>
